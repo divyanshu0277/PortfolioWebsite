4f031caa9d82f2540b7181dd2ab4867bad028c90
divyanshu raj <divyanshuraj8228@gmail.com>
2024-08-02 10:06:56 +0530

    first commit

---
+++ src/components/ThemeItem.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+
+// const ThemeItem = ({ color, img }) => {
+//   return <img src={img} alt='' className='theme__img' 
+//   onClick={() => {changeColor(color)}} />;
+// };
+
+// export default ThemeItem;
+
+
+import React from 'react';
+
+const ThemeItem = ({ color, img, changeColor }) => {
+  return <img src={img} alt='' className='theme__img' 
+  onClick={() => {changeColor(color)}} />;
+};
+
+export default ThemeItem;
